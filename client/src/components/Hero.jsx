8a47da10ed241b76771d73479bff6cb13b2dbee4
stevemarcel <just.stevemarcel@gmail.com>
2024-05-19 02:17:23 +0100

    hero text copy and styling clean up

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,16 +1,26 @@
 const Hero = () => {
   return (
-    <section className="heroBg h-96 mb-10 text-center flex items-center text-sharkLight-100">
+    <section className="heroBg h-96 mb-10  flex items-center text-sharkLight-100">
       <div className="absolute top-18 left-0 right-0 h-96 bg-sharkDark-400 bg-opacity-90 z-10"></div>{" "}
       {/* Overlay element */}
-      <div className="p-6 relative z-20">
-        <h2 className="mb-2 text-4xl font-bold">MERN AUTHENTICATION OTP SYSTEM</h2>
-        <p className="md:w-[70%] mx-auto ">
-          This is a Mern Project that focuses on Authentication with an OTP system. It sends a
-          confirmation mail for new user registrations and password updates. It stores the JWT token
-          in a HTTP-only cookie. The technologies used in the project are React via Vite, React
-          Redux Toolkit, Tailwind CSS, Express for the backend API and MongoDB for the RESTful
-          database.
+      <div className="p-6 relative z-20 md:w-[70%] mx-auto text-justify space-y-2">
+        <h2 className=" text-4xl font-bold text-center">MERN AUTHENTICATION OTP SYSTEM</h2>
+        <p className=" ">
+          This project is a comprehensive implementation of user authentication utilizing the MERN
+          (MongoDB, Express, React, Node.js) stack along with various complementary technologies.
+        </p>
+        <p>
+          It is designed to securely manage user registration, email verification, password reset
+          using OTP (One-Time Password), and token-based authentication using JWT (JSON Web Tokens).
+          The use of JWT stored in HTTP-only cookies fortifies the application against common
+          security vulnerabilities like XSS (Cross-Site Scripting) and CSRF (Cross-Site Request
+          Forgery) attacks. This not only simplifies the management of authentication tokens but
+          also ensures automated handling by web browsers, thereby mitigating the risks associated
+          with client-side JavaScript access.
+        </p>
+        <p>
+          The technologies used in the project are React via Vite, React Redux Toolkit, Tailwind
+          CSS, Express for the backend API and MongoDB for the RESTful database.
         </p>
       </div>
     </section>
