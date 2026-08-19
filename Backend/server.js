84ec052e66dcd28fba9b582ea6fe01e18b3ca134
Stephen Onyejuluwa <just.stevemarcel@gmail.com>
2026-08-19 20:26:20 +0100

    fix: Update server.js to reference 'client' instead of 'frontend' for production build

--- Backend/server.js
+++ Backend/server.js
@@ -32,11 +32,9 @@ app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "frontend/dist")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html")),
-  );
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "client", "dist", "index.html")));
 } else {
   app.get("/", (req, res) => res.send("server is ready"));
 }
