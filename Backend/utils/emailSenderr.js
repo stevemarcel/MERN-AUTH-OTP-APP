395f1f0969ce9f13045cbb6c4a2de3aa5af32077
Stephen Onyejuluwa <just.stevemarcel@gmail.com>
2026-08-21 17:55:43 +0100

    fix: Update emailSender to improve transport configuration

--- Backend/utils/emailSenderr.js
+++ Backend/utils/emailSenderr.js
@@ -4,9 +4,9 @@ const emailSender = async (email, subject, text) => {
   try {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST,
-      service: process.env.EMAIL_SERVICE,
+      // service: process.env.EMAIL_SERVICE,
       port: Number(process.env.EMAIL_PORT),
-      secure: Boolean(process.env.EMAIL_SECURE),
+      secure: process.env.EMAIL_SECURE === "true",
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD,
